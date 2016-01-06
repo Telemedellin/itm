140404ddb7247820009d16976c3914f50f2caa3f
emergugue <emergugue@gmail.com>
2016-01-05 19:41:48 -0500

    Se terminaron los templates para el loop de las categorias y los de los artículos

---
+++ wp-content/themes/itm/template-parts/content-loop-categorias.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Template part para visualizar el loop de categorías.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package itm
+ */
+
+?>
+<div class="ctn_cover-image">
+</div>
+<div class="ctn__content container">
+	<header class="ctn__header-content">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<div class="ctn__info-header">
+			<div class="ctn__breadcrumbs">
+				<span><a href="#">Home</a></span><span><a href="#">Estudiar en el ITM</a></span><span><a href="#">Programas universitarios</a></span>
+			</div><!-- ctn__breadcrumbs -->
+		</div><!-- ctn__info-header -->
+	</header><!-- ctn__header-content -->
+	<section class="ctn__section-content padding-content">
+		
+		<div class="grid-item">
+			<a href="#" class="ctn__preview">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title brd__artes-y-humanidades">
+					<h2 class="preview-title">
+						Facultades
+					</h2>
+				</div>
+			</a href="#"><!-- ctn__preview -->			
+		</div>
+		<div class="grid-item">
+			<a href="#" class="ctn__preview">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title brd__artes-y-humanidades">
+					<h2 class="preview-title">
+						Facultades
+					</h2>
+				</div>
+			</a href="#"><!-- ctn__preview -->			
+		</div>
+		<div class="grid-item">
+			<a href="#" class="ctn__preview">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title brd__artes-y-humanidades">
+					<h2 class="preview-title">
+						Facultades
+					</h2>
+				</div>
+			</a href="#"><!-- ctn__preview -->			
+		</div>
+		<div class="grid-item">
+			<a href="#" class="ctn__preview">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title brd__artes-y-humanidades">
+					<h2 class="preview-title">
+						Facultades
+					</h2>
+				</div>
+			</a href="#"><!-- ctn__preview -->			
+		</div>
+		
+	</section><!-- ctn__section-content -->
+</div>

6329f990393775878668372f80816790f172bbf7
emergugue <emergugue@gmail.com>
2016-01-13 10:56:48 -0500

    Se editó el single para los artículos

--- wp-content/themes/itm/template-parts/content-single.php
+++ wp-content/themes/itm/template-parts/content-single.php
@@ -11,8 +11,6 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
 		<div class="entry-meta">
 			<?php itm_posted_on(); ?>
 		</div><!-- .entry-meta -->

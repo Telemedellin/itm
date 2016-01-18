c17e7ec446d5e685cf90ca84a2dce501d4c11653
emergugue <emergugue@gmail.com>
2016-01-18 16:29:02 -0500

    Se maquetó la previsualización de las noticias para el paddon de VC

---
+++ wp-content/themes/itm/template-parts/content-preview-news.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Template part for displaying page content in page.php.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package itm
+ */
+
+?>
+
+<div class="ctn__preview-grid">
+	<div class="ctn__preview-grid__primary-col">
+		<div class="grid-item">
+			<a href="#" class="ctn__preview ctn__preview-news">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title">
+					<h2 class="preview-title">
+						Título del artículo o de la noticia, que puede ser un poco largo
+					</h2>
+				</div>
+				<div class="ctn__preview-news_date">
+					<span class="preview-news_date">31 de mayo de 2016</span>
+				</div>
+			</a href="#"><!-- ctn__preview -->
+		</div><!-- /grid-item -->
+	</div>
+	<div class="ctn__preview-grid__secondary-col">
+		<div class="grid-item">
+			<a href="#" class="ctn__preview ctn__preview-news">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title">
+					<h2 class="preview-title">
+						Título del artículo o de la noticia, que puede ser un poco largo
+					</h2>
+				</div>
+				<div class="ctn__preview-news_date">
+					<span class="preview-news_date">31 de mayo de 2016</span>
+				</div>
+			</a href="#"><!-- ctn__preview -->
+		</div><!-- /grid-item -->
+		<div class="grid-item">
+			<a href="#" class="ctn__preview ctn__preview-news">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title">
+					<h2 class="preview-title">
+						Título del artículo o de la noticia, que puede ser un poco largo
+					</h2>
+				</div>
+				<div class="ctn__preview-news_date">
+					<span class="preview-news_date">31 de mayo de 2016</span>
+				</div>
+			</a href="#"><!-- ctn__preview -->
+		</div><!-- /grid-item -->
+		<div class="grid-item">
+			<a href="#" class="ctn__preview ctn__preview-news">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title">
+					<h2 class="preview-title">
+						Título del artículo o de la noticia, que puede ser un poco largo
+					</h2>
+				</div>
+				<div class="ctn__preview-news_date">
+					<span class="preview-news_date">31 de mayo de 2016</span>
+				</div>
+			</a href="#"><!-- ctn__preview -->
+		</div><!-- /grid-item -->
+		<div class="grid-item">
+			<a href="#" class="ctn__preview ctn__preview-news">
+				<div class="ctn__preview-image" style="background: url(http://lorempixel.com/400/400) no-repeat; background-size: 100%; background-position: center center">
+					<img src="" alt="" class="preview-image">
+				</div>
+				<div class="ctn__preview-title">
+					<h2 class="preview-title">
+						Título del artículo o de la noticia, que puede ser un poco largo
+					</h2>
+				</div>
+				<div class="ctn__preview-news_date">
+					<span class="preview-news_date">31 de mayo de 2016</span>
+				</div>
+			</a href="#"><!-- ctn__preview -->
+		</div><!-- /grid-item -->
+	</div>
+</div>
+
+
+
